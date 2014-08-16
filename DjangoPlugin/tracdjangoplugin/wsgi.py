891fd2552df194d02c45167e07ee46820a9c3ab9
Florian Apolloner <florian@apolloner.eu>
2014-08-16 15:53:31 +0200

    Removed top level wsgi file.

--- DjangoPlugin/tracdjangoplugin/wsgi.py
+++ DjangoPlugin/tracdjangoplugin/wsgi.py
@@ -1,5 +1,15 @@
+import json
+import os
+
 import trac.web.main
+application = trac.web.main.dispatch_request
 
-from tracdjangoplugin import djangoauth
+# Add GitHub authentication
+with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'oauth2-secrets.json')) as f:
+    oauth2_secrets = json.loads(f.read())
+oauth2_secrets = dict((str(k), str(v)) for (k, v) in oauth2_secrets.items())
 
-application = djangoauth.DjangoAuth(trac.web.main.dispatch_request)
+from wsgioauth2 import github
+oauth2_client = github.make_client(client_id=oauth2_secrets['client_id'], client_secret=oauth2_secrets['client_secret'])
+
+application = oauth2_client.wsgi_middleware(application, path='/oauth2/', login_path='/login', set_remote_user=True, secret=oauth2_secrets['hmac_secret'])
