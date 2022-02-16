d9215a7f39e21abfaf74fb402e7a0165defe2893
Tobias McNulty <tobias@caktusgroup.com>
2022-02-16 23:34:24 +0000

    conditionally collect trac static

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Fail loudly, don't echo variables to logs
+set -e
+
 # Trac doesn't read environment variables, so allow setting arbitrary lines in
 # trac.ini from the environment before Trac starts. E.g.,
 #   export TRAC_INI_database = postgres://...
@@ -10,7 +13,9 @@ for var in "${!TRAC_INI_@}"; do
     sed -i "s;^${var:9} = .*;${var:9} = ${!var};" trac-env/conf/trac.ini
 done
 
-# Collect trac static files (to be served by nginx)
-/venv/bin/trac-admin ./trac-env/ deploy ./static/
+if [ "x$TRAC_COLLECT_STATIC" = 'xon' ]; then
+    # Collect trac static files to be served by nginx
+    /venv/bin/trac-admin ./trac-env/ deploy ./static/
+fi
 
 exec "$@"
