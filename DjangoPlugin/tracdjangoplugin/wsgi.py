7a333bb14745e74a8296dbc840abe619340aba31
Florian Apolloner <florian@apolloner.eu>
2022-11-01 10:21:50 +0100

    Speedup Trac's git calls.

--- DjangoPlugin/tracdjangoplugin/wsgi.py
+++ DjangoPlugin/tracdjangoplugin/wsgi.py
@@ -1,5 +1,10 @@
 import trac.web.main
 application = trac.web.main.dispatch_request
 
+# Massive hack to make Trac fast, otherwise every git call tries to close ulimit -n (1e6) fds
+# Python 3 would perform better here, but we are still on 2.7 for Trac, so leak fds for now.
+from tracopt.versioncontrol.git import PyGIT
+PyGIT.close_fds = False
+
 from .djangoauth import DjangoAuth
 application = DjangoAuth(application)
