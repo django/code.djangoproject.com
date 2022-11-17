3e19978d75b96a64b16064e9a92063e1f92e41fd
Tobias McNulty <tobias@caktusgroup.com>
2022-11-17 01:42:17 +0000

    Add Sentry monitoring

--- DjangoPlugin/tracdjangoplugin/wsgi.py
+++ DjangoPlugin/tracdjangoplugin/wsgi.py
@@ -1,4 +1,10 @@
+import os
+
+import sentry_sdk
 import trac.web.main
+from sentry_sdk.integrations.wsgi import SentryWsgiMiddleware
+
+
 application = trac.web.main.dispatch_request
 
 # Massive hack to make Trac fast, otherwise every git call tries to close ulimit -n (1e6) fds
@@ -8,3 +14,16 @@ PyGIT.close_fds = False
 
 from .djangoauth import DjangoAuth
 application = DjangoAuth(application)
+
+trac_dsn = os.getenv("SENTRY_DSN")
+
+if trac_dsn:
+    sentry_sdk.init(
+        dsn=trac_dsn,
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production,
+        traces_sample_rate=1.0,
+    )
+    application = SentryWsgiMiddleware(application)
