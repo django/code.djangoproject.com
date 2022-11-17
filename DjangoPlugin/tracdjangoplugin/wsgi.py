a339c48ff068d14214adc8a70816b5da31aa3d8c
Tobias McNulty <tobias@caktusgroup.com>
2022-11-17 02:18:18 +0000

    reduce sampling rate

--- DjangoPlugin/tracdjangoplugin/wsgi.py
+++ DjangoPlugin/tracdjangoplugin/wsgi.py
@@ -22,6 +22,6 @@ if trac_dsn:
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production,
-        traces_sample_rate=1.0,
+        traces_sample_rate=0.2,
     )
     application = SentryWsgiMiddleware(application)
